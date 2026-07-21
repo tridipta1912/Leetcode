d217b827f4eaccc5245053130ef245394e806536
tridipta1912 <tridipta1912@gmail.com>
2026-07-21 18:23:18 +0530

    Time: 28 ms (49.01%), Space: 23.1 MB (31.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,8 +3,7 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
+    
         vector<int> dx = {0, 0, -1, 1};
         vector<int> dy = {1, -1, 0, 0};
 
@@ -13,11 +12,11 @@ public:
         {
             for(int j = 0; j < m; j++)
             {
-                if(!vis[i][j] && grid[i][j] == '1')
+                if(grid[i][j] == '1')
                 {
                     queue<array<int, 2>> q;
                     q.push({i, j});
-                    vis[i][j] = true;
+                    grid[i][j] = '0';
                     ans++;
                     while(!q.empty())
                     {
@@ -27,8 +26,8 @@ public:
                         {
                             int nx = x + dx[i];
                             int ny = y + dy[i];
-                            if(nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny] || grid[nx][ny] == '0')   continue;
-                            vis[nx][ny] = 1;
+                            if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] == '0')   continue;
+                            grid[nx][ny] = '0';
                             q.push({nx, ny});
                         }
                     }
