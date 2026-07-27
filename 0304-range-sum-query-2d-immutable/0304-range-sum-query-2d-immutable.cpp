d76b16567c9e5b1f4e04e1b8074eaf65327be1db
tridipta1912 <tridipta1912@gmail.com>
2026-07-27 21:26:06 +0530

    Time: 23 ms (63.75%), Space: 149.2 MB (62.24%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,30 +1,19 @@
 class NumMatrix {
 public:
-    vector<vector<int>> P;
+    vector<vector<int>> prefix;
     NumMatrix(vector<vector<int>>& matrix) {
-        P.resize(matrix.size(), vector<int>(matrix[0].size()));
+        prefix.resize(matrix.size() + 1, vector<int>(matrix[0].size() + 1));
         for(int i = 0; i < matrix.size(); i++)
         {
             for(int j = 0; j < matrix[0].size(); j++)
             {
-                P[i][j] = matrix[i][j];
-                if(i == 0 && j == 0)    continue;
-                if(i)   P[i][j] += P[i - 1][j];
-                if(j)   P[i][j] += P[i][j - 1];
-                if(i && j) P[i][j] -= P[i - 1][j - 1];
+                prefix[i + 1][j + 1] = matrix[i][j] + prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        if(row1 > row2) swap(row1, row2);
-        if(col1 > col2) swap(col1, col2);
-
-        int ans = P[row2][col2];
-        if(row1)    ans -= P[row1 - 1][col2];
-        if(col1)    ans -= P[row2][col1 - 1];
-        if(row1 && col1)    ans += P[row1 - 1][col1 - 1];
-        
+        int ans = prefix[row2 + 1][col2 + 1] - prefix[row1][col2 + 1] - prefix[row2 + 1][col1] + prefix[row1][col1];
         return ans;
     }
 };
