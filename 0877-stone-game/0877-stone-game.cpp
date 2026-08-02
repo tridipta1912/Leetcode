56d2e5b37ce8d81751903c4db49d33e50df51f67
tridipta1912 <tridipta1912@gmail.com>
2026-08-02 10:56:14 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1e9));
-        int tot = 0;
-        for(int len = 1; len <= n; len++)
-        {
-            for(int i = 0; i + len < n; i++)
-            {
-                int j = i + len;
-                if(j)   dp[i][j] = max(dp[i][j], piles[j] - dp[i][j - 1]);
-                if(i + 1 < n)   dp[i][j] = max(dp[i][j], piles[i] - dp[i + 1][j]);
-            }
-        }
-        return (2 * dp[0][n - 1] > tot);
+        return true;
     }
 };
