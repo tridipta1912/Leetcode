bf35899580684dd4aa77bfc8222e9086bc0b25e3
tridipta1912 <tridipta1912@gmail.com>
2026-08-17 14:54:24 +0530

    Time: 4 ms (24.84%), Space: 13.3 MB (54.68%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> par(n + 1, 0);
+        vector<int> sz(n + 1, 1);
+        iota(par.begin(), par.end(), 0);
+
+        auto find = [&](auto& self, int x) -> int
+        {
+            if(par[x] == x) return x;
+            return ((par[x] = self(self, par[x])));
+        };
+
+        auto unite = [&](int u, int v)
+        {
+            int ru = find(find, u);
+            int rv = find(find, v);
+
+            if(ru == rv)    return false;
+
+            if(sz[ru] < sz[rv]) swap(ru, rv);
+
+            sz[ru] += sz[rv];
+            par[rv] = ru;
+
+            return true;
+        };
+
+        vector<int> ans;
+        for(auto x : edges)
+        {
+            if(!unite(x[0], x[1]))
+            {
+                ans = {x[0], x[1]};
+            }
+        }
+
+        return ans;
+    }
+};
