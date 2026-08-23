5c514a8ad7894f63e4224c9758490cd66b30e935
tridipta1912 <tridipta1912@gmail.com>
2026-08-24 02:59:07 +0530

    Time: 3 ms (63.33%), Space: 13.5 MB (23.89%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.cpp
@@ -12,24 +12,18 @@ public:
                 for(ll k = 0; k < n; k++)   cost[i][j] += (students[i][k] == mentors[j][k]);
             }
         }
-        ll M = (1LL << (2 * m));
+        ll M = (1LL << (m));
         vector<ll> dp(M);
         for(ll msk = 0; msk < M; msk++)
         {
-            for(ll i = 0; i < m; i++)
+            ll i = __builtin_popcount(msk);
+            for(ll j = 0; j < m; j++)
             {
-                for(ll j = 0; j < m; j++)
-                {
-                    ll bitt = (1LL << m) * (1LL << i);
-                    bitt |= (1LL << j);
-                    if((msk & bitt) == 0)   
-                    {
-                        dp[msk | bitt] = max(dp[msk | bitt], dp[msk] + cost[i][j]);
-                    }
-                }
+                if(msk & (1LL << j))    continue;
+                dp[msk | (1LL << j)] = max(dp[msk | (1LL << j)], dp[msk] + cost[i][j]);
             }
         }
 
-        return *max_element(dp.begin(), dp.end());
+        return dp.back();
     }
 };
