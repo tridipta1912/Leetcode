fcb1b2bca0eb610f27d321e2ecbfec20768c706d
tridipta1912 <tridipta1912@gmail.com>
2026-08-25 04:14:11 +0530

    Time: 114 ms (35.07%), Space: 87.2 MB (84.35%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        using ll = long long;
+        constexpr ll INF = 1e18;
+        ll n = stones.size();
+        for(ll i = 1; i < n; i++)   stones[i] += stones[i - 1];
+        
+        ll ans = stones.back();
+        for(ll i = n - 2; i >= 1; i--)
+        {
+            ans = max(ans, stones[i] - ans);
+        }
+        return ans;
+    }
+};
