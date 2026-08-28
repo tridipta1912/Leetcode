fb4af9f8f2e752a7410c349b9ab91760aadffcb3
tridipta1912 <tridipta1912@gmail.com>
2026-08-28 23:39:36 +0530

    Time: 758 ms (7.85%), Space: 275.1 MB (5.06%) - LeetHub

---
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int target) {
+        using ll = long long;
+        ll n = matrix.size();
+        ll m = matrix[0].size();
+
+        vector<vector<ll>> matrix1;
+        if(m < n)
+        {
+            matrix1.assign(m, vector<ll>(n, 0));
+            for(ll i = 0; i < n; i++)   for(ll j = 0; j < m; j++)   matrix1[j][i] = matrix[i][j];
+            swap(m, n);
+        }
+        else
+        {
+            matrix1.assign(n, vector<ll>(m, 0));
+            for(ll i = 0; i < n; i++)
+            {
+                for(ll j = 0; j < m; j++)   matrix1[i][j] = matrix[i][j];
+            }
+        }
+
+        for(ll i = 0; i < m; i++)
+        {
+            for(ll j = 1; j < n; j++)   matrix1[j][i] += matrix1[j - 1][i];
+        }
+
+        ll ans = -1e18;
+        for(ll i = 0; i < n; i++)
+        {
+            for(ll j = i; j < n; j++)
+            {
+                vector<ll> st(m);
+                for(ll k = 0; k < m; k++)   st[k] = (matrix1[j][k] - (i ? matrix1[i - 1][k] : 0));
+                set<ll> s;
+                s.insert(0);
+
+                for(ll k = 0; k < m; k++)   
+                {
+                    if(k)   st[k] += st[k - 1];
+                    auto it = s.lower_bound(st[k] - target);
+                    if(it != s.end())   ans = max(ans, st[k] - *it);
+                    s.insert(st[k]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
