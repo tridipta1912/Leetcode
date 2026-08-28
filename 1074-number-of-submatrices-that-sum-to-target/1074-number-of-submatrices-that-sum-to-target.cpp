90cd89d5a8510f677ee3df23cd62da57b6831e59
tridipta1912 <tridipta1912@gmail.com>
2026-08-28 23:33:05 +0530

    Time: 700 ms (17.64%), Space: 331.4 MB (5.14%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -4,9 +4,26 @@ public:
         using ll = long long;
         ll n = matrix.size();
         ll m = matrix[0].size();
+
+        vector<vector<ll>> matrix1;
+        if(m < n)
+        {
+            matrix1.assign(m, vector<ll>(n, 0));
+            for(ll i = 0; i < n; i++)   for(ll j = 0; j < m; j++)   matrix1[j][i] = matrix[i][j];
+            swap(m, n);
+        }
+        else
+        {
+            matrix1.assign(n, vector<ll>(m, 0));
+            for(ll i = 0; i < n; i++)
+            {
+                for(ll j = 0; j < m; j++)   matrix1[i][j] = matrix[i][j];
+            }
+        }
+
         for(ll i = 0; i < m; i++)
         {
-            for(ll j = 1; j < n; j++)   matrix[j][i] += matrix[j - 1][i];
+            for(ll j = 1; j < n; j++)   matrix1[j][i] += matrix1[j - 1][i];
         }
         ll ans = 0;
         for(ll i = 0; i < n; i++)
@@ -14,7 +31,7 @@ public:
             for(ll j = i; j < n; j++)
             {
                 vector<ll> st(m);
-                for(ll k = 0; k < m; k++)   st[k] = (matrix[j][k] - (i ? matrix[i - 1][k] : 0));
+                for(ll k = 0; k < m; k++)   st[k] = (matrix1[j][k] - (i ? matrix1[i - 1][k] : 0));
                 unordered_map<ll, ll> mp;
                 mp[0] = 1;
                 for(ll k = 0; k < m; k++)   
