7e512a8fab0208499a453ef128d6db907889f143
tridipta1912 <tridipta1912@gmail.com>
2026-09-07 13:18:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.74%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -5,18 +5,18 @@ public:
         constexpr ll MOD = 1e9 + 7;
         ll n = s.length();
         vector<ll> dp(n + 1, 0);
+        vector<ll> lst(26, 0);
         dp[0] = 1;
         for(ll i = 1; i <= n; i++)
         {
-            for(ll j = i - 1; j >= 0; j--)
-            {
-                dp[i] += dp[j];
-                dp[i] %= MOD;
-                if(j && s[j - 1] == s[i - 1])   break;
-            }
+            dp[i] += dp[i - 1];
+            dp[i] %= MOD;
+            if(lst[s[i - 1] - 'a'] > 0)   dp[i] -= dp[lst[s[i - 1] - 'a'] - 1];
+            dp[i] = (dp[i] % MOD + MOD) % MOD;
+            dp[i] += dp[i - 1];
+            dp[i] %= MOD;
+            lst[s[i - 1] - 'a'] = i;
         }
-        ll ans = 0;
-        for(ll i = 1; i <= n; i++)  ans += dp[i], ans %= MOD;
-        return (int)(ans);
+        return (int)((dp.back() - 1 + MOD) % MOD);
     }
 };
