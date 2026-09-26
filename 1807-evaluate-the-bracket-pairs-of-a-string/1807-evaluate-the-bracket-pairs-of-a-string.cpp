6b0a8134ceac2a74ce40a72e219ccb9a18a72322
tridipta1912 <tridipta1912@gmail.com>
2026-09-26 12:09:32 +0530

    Time: 126 ms (25.86%), Space: 144.9 MB (6.23%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        string cur = "", ans = "";
+        unordered_map<string, string> mp;
+        for(auto x : knowledge) mp[x[0]] = x[1];
+        bool is_open = false;
+        for(auto x : s)
+        {
+            if(x == '(')    is_open = true;
+            else if(x == ')')   
+            {
+                ans += ((mp.find(cur) == mp.end()) ? "?" : mp[cur]);
+                is_open = false;
+                cur = "";
+            }
+            else
+            {
+                if(is_open) cur += x;
+                else
+                {
+                    ans += x;
+                }
+            }
+        }
+        return ans;
+    }
+};
